6ca3b9e297c88bee487ad201d90a67e4266789e7
kristian <kristianfagerstrom7@gmail.com>
2023-03-22 14:13:38 +0200

    resolved

--- Labwork-2/MyApp24/labwork-3-1/src/app/songs/home.module.ts
+++ Labwork-2/MyApp24/labwork-3-1/src/app/songs/home.module.ts
@@ -2,10 +2,10 @@ import { IonicModule } from '@ionic/angular';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { Tab2Page } from './tab2.page';
+import { tab2Page } from './tab2.page.htlm';
 import { ExploreContainerComponentModule } from '../explore-container/explore-container.module';
 
-import { Tab2PageRoutingModule } from './tab2-routing.module';
+import { homePageRoutingModule } from './tab2-routing.module.ts';
 
 @NgModule({
   imports: [
@@ -13,8 +13,8 @@ import { Tab2PageRoutingModule } from './tab2-routing.module';
     CommonModule,
     FormsModule,
     ExploreContainerComponentModule,
-    Tab2PageRoutingModule
+    homePageRoutingModule
   ],
-  declarations: [Tab2Page]
+  declarations: [tab2Page]
 })
-export class Tab2PageModule {}
+export class homePageModule {}
--- Labwork-2/MyApp24/labwork-3-1/src/app/songs/tab2.page.ts
+++ Labwork-2/MyApp24/labwork-3-1/src/app/songs/tab2.page.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-tab2',
+  selector: 'app-home',
   templateUrl: 'tab2.page.html',
   styleUrls: ['tab2.page.scss']
 })
-export class Tab2Page {
+export class HomePage {
 
   constructor() {}
 
