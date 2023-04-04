daca38187d464152208a13f32fea477f18d78536
kristian <kristianfagerstrom7@gmail.com>
2023-04-05 01:12:45 +0300

    Added firebase

---
+++ Labwork-2/MyApp24/firebase/src/environments/environment.prod.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  firebase: {
+    projectId: 'labwork-4-cc43f',
+    appId: '1:1037547551682:web:49eda66c2e01ffde7a345b',
+    storageBucket: 'labwork-4-cc43f.appspot.com',
+    locationId: 'europe-west',
+    apiKey: 'AIzaSyAaWXBWheiTS9mCFI-AOqW3ZT8Au7KQRxc',
+    authDomain: 'labwork-4-cc43f.firebaseapp.com',
+    messagingSenderId: '1037547551682',
+  },
+  production: true
+};
