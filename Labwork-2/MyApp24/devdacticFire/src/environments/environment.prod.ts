ad3bd15ea2bef094db0708fef5aa377f76c62742
kristian <kristianfagerstrom7@gmail.com>
2023-04-04 18:47:00 +0300

    Trying to solve bugs

--- Labwork-2/MyApp24/devdacticFire/src/environments/environment.prod.ts
+++ Labwork-2/MyApp24/devdacticFire/src/environments/environment.prod.ts
@@ -1,11 +1,11 @@
 export const environment = {
-	production: false,
+production: false,
 	firebase: {
-		apiKey: '',
-		authDomain: '',
-		projectId: '',
-		storageBucket: '',
-		messagingSenderId: '',
-		appId: ''
+		apiKey: 'AIzaSyAaWXBWheiTS9mCFI-AOqW3ZT8Au7KQRxc',
+		authDomain: 'labwork-4-cc43f.firebaseapp.com',
+		projectId: 'labwork-4-cc43f',
+		storageBucket: 'labwork-4-cc43f.appspot.com',
+		messagingSenderId: '1037547551682',
+		appId: '1:1037547551682:web:49eda66c2e01ffde7a345b'
 	}
 };
