2ca80b4e730445a9da3707e689340a314f4eb656
kristian <kristianfagerstrom7@gmail.com>
2023-04-05 01:19:56 +0300

    models added

--- Labwork-2/MyApp24/firebase/src/app/app.routes.ts
+++ Labwork-2/MyApp24/firebase/src/app/app.routes.ts
@@ -5,4 +5,12 @@ export const routes: Routes = [
     path: '',
     loadChildren: () => import('./tabs/tabs.routes').then((m) => m.routes),
   },
+  {
+    path: 'home',
+    loadComponent: () => import('./home/home.page').then( m => m.HomePage)
+  },
+  {
+    path: 'models',
+    loadComponent: () => import('./models/models.page').then( m => m.ModelsPage)
+  },
 ];
